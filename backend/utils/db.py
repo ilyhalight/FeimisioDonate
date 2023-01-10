b601df58c92ee7082142178ea408acb6eaeb7c66
Toil <toil.official@yandex.com>
2023-01-10 21:15:18 +0300

    added promocodes

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -4,6 +4,7 @@ import logging
 
 from sql.privillege.controller import DbPrivillegeController
 from sql.privillege_info.controller import DbPrivillegeInfoController
+from sql.promocodes.controller import DbPromocodesController
 
 log = logging.getLogger('worker')
 
@@ -13,6 +14,7 @@ async def save_privilleges_to_json():
     privilleges = await DbPrivillegeController().get_all()
     async with aiofiles.open('config/privilleges.json', 'w', encoding = 'utf-8') as f:
         await f.write(json.dumps(privilleges))
+        log.info('privilleges saved to json')
         await f.close()
 
 async def get_privilleges_json():
@@ -38,6 +40,7 @@ async def save_privilleges_info_to_json():
     privilleges = await DbPrivillegeInfoController().get_all()
     async with aiofiles.open('config/privilleges_info.json', 'w', encoding = 'utf-8') as f:
         await f.write(json.dumps(privilleges))
+        log.info('privilleges info saved to json')
         await f.close()
 
 async def get_privilleges_info_json():
@@ -58,3 +61,29 @@ async def find_privilleges_info_json(link: str):
             if privillege['link'] == link:
                 res.append(privillege)
     return res
+
+async def save_promocodes_to_json():
+    """Сохраняет доступные промокоды из базы данных в json файл"""
+    promocodes = await DbPromocodesController().get_all()
+    async with aiofiles.open('config/promocodes.json', 'w', encoding = 'utf-8') as f:
+        await f.write(json.dumps(promocodes))
+        log.info('promocodes saved to json')
+        await f.close()
+
+async def get_promocodes_json():
+    """Возвращает доступные промокоды из json файла"""
+    try:
+        async with aiofiles.open('config/promocodes.json', 'r', encoding = 'utf-8') as f:
+            promocodes = await f.read()
+            return json.loads(promocodes)
+    except FileNotFoundError:
+        return []
+
+async def find_promocodes_json(key: str):
+    """Возвращает информацию о проомокоде из json файла по uid"""
+    promocodes = await get_promocodes_json()
+    if promocodes:
+        for promocode in promocodes:
+            if promocode['key'] == key:
+                return promocode
+    return []
