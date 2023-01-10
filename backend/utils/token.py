0c8c06b987b9dedb3e84777d29959a40c5ba1114
Toil <toil.official@yandex.com>
2023-01-10 21:16:11 +0300

    renamed token to string

--- backend/utils/token.py
+++ backend/utils/token.py
@@ -3,15 +3,15 @@ import time
 from hashlib import sha256
 
 
-async def generate_token(privillege: str, timestamp: int):
-    token_pre = f'{privillege}{os.environ.get("SECRET_KEY")}{timestamp}'
+async def generate_token(string: str, timestamp: int):
+    token_pre = f'{string}{os.environ.get("FEIMISIO_TOKEN")}{timestamp}'
     token = sha256(token_pre.encode('utf-8')).hexdigest()
     return token
 
-async def validate_token(token: str, privillege: str, timestamp: int):
+async def validate_token(token: str, string: str, timestamp: int):
     if int(time.time()) > timestamp + 12:
         return False
-    token_generated = await generate_token(privillege, timestamp)
+    token_generated = await generate_token(string, timestamp)
     if token == token_generated:
         return True
     return False
