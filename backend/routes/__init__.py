0c20bfbcf82cd68f0a7fc7c9280a2138f5fb5cd8
Toil <toil.official@yandex.com>
2023-01-10 21:13:13 +0300

    added prefix

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from routes.endpoints import api_privilleges, api_methods, api_callbacks
+from routes.endpoints import api_privilleges, api_promocodes, api_methods, api_callbacks
 
-api_router = APIRouter()
+api_router = APIRouter(prefix = "/api")
 api_router.include_router(api_privilleges.router, tags = ['Privilleges'])
+api_router.include_router(api_promocodes.router, tags = ['Promocodes'])
 api_router.include_router(api_methods.router, tags = ['Methods'])
-api_router.include_router(api_callbacks.router, tags = ['Callbacks'])
+api_router.include_router(api_callbacks.router, tags = ['Callbacks'], prefix = "/callback")
 
