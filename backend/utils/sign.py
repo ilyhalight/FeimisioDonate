5e3f5b950ca6105c854e1f14ab5129f2a6927c49
Toil <toil.official@yandex.com>
2023-01-12 02:23:35 +0300

    added sign sha1 for crystalpay

--- backend/utils/sign.py
+++ backend/utils/sign.py
@@ -1,5 +1,8 @@
-from hashlib import md5
+from hashlib import md5, sha1
 
 
-def sign(shop_id: int, amount: int|float, secret: str, order_id: int|str):
+def sign_md5(shop_id: int, amount: int|float, secret: str, order_id: int|str):
     return md5(f'{shop_id}:{amount}:{secret}:{order_id}'.encode('utf-8')).hexdigest()
+
+def sign_sha1(pay_id: str, currency: str, secret2: str):
+    return sha1(f'{pay_id}:{currency}:{secret2}'.encode('utf-8')).hexdigest()
