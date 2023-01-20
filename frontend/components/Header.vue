3cb964be65c8c6c186eb3b55d06776d11a3daf94
Toil <toil.official@yandex.com>
2023-01-20 20:20:39 +0300

    added github link

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -13,6 +13,9 @@
       </div>
       <div class="navbar-end">
         <ThemeSelector/>
+        <a href="https://github.com/ilyhalight/FeimisioDonate/" class="btn_ghost max-sm:m-2" target="_blank">
+          <img class="p-1" alt="Github" src="~/assets/images/social/github.svg" width="36" height="36"/>
+        </a>
       </div>
     </div>
   </header>
