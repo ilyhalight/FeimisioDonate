a92c0edecde4da3b4109d8df6342bc2a2ce0c5ff
Toil <toil.official@yandex.com>
2023-03-16 00:56:16 +0300

    added hmac sha256 & md5 upper sign

--- backend/utils/sign.py
+++ backend/utils/sign.py
@@ -1,12 +1,17 @@
-from hashlib import md5, sha1
+import hmac
+import json
+from hashlib import md5, sha1, sha256
 
 
 def sign_md5(shop_id: int, amount: int|float, secret: str, order_id: int|str):
     return md5(f'{shop_id}:{amount}:{secret}:{order_id}'.encode('utf-8')).hexdigest()
 
-# Crystalapi v1
-# def sign_sha1(pay_id: str, currency: str, secret2: str):
-#     return sha1(f'{pay_id}:{currency}:{secret2}'.encode('utf-8')).hexdigest()
+def sign_md5_upper(amount: int|float, order_id: str, secret: str):
+    return md5(f'{amount}:{order_id}:{secret}'.upper().encode('utf-8')).hexdigest()
 
 def sign_sha1(pay_id: str, secret2: str):
     return sha1(f'{pay_id}:{secret2}'.encode('utf-8')).hexdigest()
+
+def sign_hmac_sha256(data: dict, secret: str):
+    json_str = json.dumps(data).encode()
+    return hmac.new(bytes(secret, 'UTF-8'), json_str, sha256).hexdigest()
