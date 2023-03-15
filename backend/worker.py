1bca8f6f280cb4d8cbe194132b0649f6dadceb5a
Toil <toil.official@yandex.com>
2023-03-16 00:59:41 +0300

    renamed privillege -> privilege, added payment systems cachify

--- backend/worker.py
+++ backend/worker.py
@@ -3,11 +3,12 @@ import logging
 from fastapi_utils.tasks import repeat_every
 
 from core.app import app
-from sql.privillege.controller import DbPrivillegeController
-from sql.privillege_info.controller import DbPrivillegeInfoController
+from sql.privilege.controller import DbPrivilegeController
+from sql.privilege_info.controller import DbPrivilegeInfoController
 from sql.promocode_uses.controller import DbPromocodeUsesController
 from sql.promocodes.controller import DbPromocodesController
-from utils.db import save_privilleges_to_json, save_privilleges_info_to_json, save_promocodes_to_json
+from sql.payment_systems.controller import DbPaymentSystemsController
+from utils.db import save_privileges_to_json, save_privileges_info_to_json, save_promocodes_to_json, save_payment_systems_to_json
 
 
 log = logging.getLogger('worker')
@@ -16,15 +17,17 @@ def init_worker():
     @app.on_event('startup')
     async def startup():
         log.info('Worker started')
-        await DbPrivillegeController().init()
-        await DbPrivillegeInfoController().init()
+        await DbPrivilegeController().init()
+        await DbPrivilegeInfoController().init()
         await DbPromocodesController().init()
         await DbPromocodeUsesController().init()
+        await DbPaymentSystemsController().init()
         log.info('DB initialized')
 
     @app.on_event("startup")
     @repeat_every(seconds = 60 * 5)  # 5 min
     async def worker():
-        await save_privilleges_to_json()
-        await save_privilleges_info_to_json()
+        await save_privileges_to_json()
+        await save_privileges_info_to_json()
         await save_promocodes_to_json()
+        await save_payment_systems_to_json()
