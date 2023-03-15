17a417941c038532e6c535d2a90e7095ba5e1c6d
Toil <toil.official@yandex.com>
2023-03-16 01:01:56 +0300

    added support of anypay, lava, paypalych

--- backend/routes/endpoints/api_callbacks.py
+++ backend/routes/endpoints/api_callbacks.py
@@ -4,8 +4,10 @@ from fastapi import APIRouter, status, Form, Header
 from fastapi.responses import JSONResponse
 
 from models.crystalpay_request import CrystalPayRequest
-from utils.give import give_privillege_callback
-from utils.sign import sign_md5, sign_sha1
+from models.lava_request import LavaRequest
+from utils.give import give_privilege_callback
+from utils.sign import sign_md5, sign_sha1, sign_hmac_sha256, sign_md5_upper
+from utils.utils import sort_dict
 
 router = APIRouter()
 log = logging.getLogger('server')
@@ -86,20 +88,17 @@ async def index(
         REMOTE_ADDR 	IP адрес
 
     """
-    log.info(f'Callback for freekassa payments: {locals()}')
     if str(MERCHANT_ID) == os.environ.get('FREEKASSA_SHOPID'):
         server_sign = sign_md5(MERCHANT_ID, AMOUNT, os.environ.get('FREEKASSA_SECRET'), MERCHANT_ORDER_ID)
         if server_sign != SIGN:
             return JSONResponse(content = {'error': 'Wrong SIGN'}, status_code = status.HTTP_403_FORBIDDEN)
-        log.debug('SIGN is valid')
         if str(AMOUNT) != str(us_price):
             return JSONResponse(content = {'error': 'Incorrect payment amount'}, status_code = status.HTTP_403_FORBIDDEN)
-        log.debug('AMOUNT is valid')
         if str(CUR_ID) in currencies:
             currency = currencies[str(CUR_ID)]
         else:
             currency = 'Unknown'
-        return await give_privillege_callback('FreeKassa', us_uid, us_steamLink, AMOUNT, commission, currency, us_promoCode)
+        return await give_privilege_callback('FreeKassa', us_uid, us_steamLink, AMOUNT, commission, currency, us_promoCode)
     return JSONResponse(content = {'error': 'Request from not availabled IP or MERCHANT_ID is wrong'}, status_code = status.HTTP_403_FORBIDDEN)
 
 @router.post('/crystalpay', summary = 'Callback for crystalpay payments')
@@ -108,7 +107,7 @@ async def index(data: CrystalPayRequest):
 
     if data.id and data.currency and data.signature:
         if data.state != 'payed':
-            log.debug('privillege not yes been paid')
+            log.debug('privilege not yes been paid')
             return JSONResponse(content = {'error': 'Privilege has not yet been paid'}, status_code = status.HTTP_402_PAYMENT_REQUIRED)
 
         if data.type != 'purchase':
@@ -128,8 +127,7 @@ async def index(data: CrystalPayRequest):
             return JSONResponse(content = {'error': 'Incorrect payment amount'}, status_code = status.HTTP_403_FORBIDDEN)
         p_steam_link = data.extra.split(',')[2]
         p_promo_code = data.extra.split(',')[3]
-        return await give_privillege_callback('CrystalPay', p_uid, p_steam_link, data.amount, data.service_commission, data.method, p_promo_code)
-    log.debug('request is wrong')
+        return await give_privilege_callback('CrystalPay', p_uid, p_steam_link, data.amount, data.service_commission, data.method, p_promo_code)
     return JSONResponse(content = {'error': 'Request is wrong'}, status_code = status.HTTP_403_FORBIDDEN)
 
 @router.post('/enot', summary = 'Callback for enot payments')
@@ -141,7 +139,7 @@ async def index(
     """Callback for enot payments
 
     Args:
-        merchant (int): ID вашего магазина  
+        merchant (str): ID вашего магазина  
         amount (float): Сумма заказа
         credited (float): Сумма зачисленная вам на баланс (В рублях)
         intid (int): ID операции в нашей системе
@@ -155,10 +153,6 @@ async def index(
         custom_field (list|dict|tuple): Строка или массив который вы передавали в параметр "cf"
     """
     if str(merchant) == os.environ.get('ENOT_SHOPID'):
-        log.debug(credited)
-        log.debug(commission)
-        log.debug(custom_field)
-        log.debug('SIGN is valid')
         server_sign_2 = sign_md5(merchant, amount, os.environ.get('ENOT_SECRET2'), merchant_id)
         if server_sign_2 != sign_2:
             log.debug('wrong sign 2 ')
@@ -174,6 +168,125 @@ async def index(
         p_steam_link = custom_field.split(',')[2]
         p_promo_code = custom_field.split(',')[3]  
         
-        return await give_privillege_callback('Enot.io', p_uid, p_steam_link, new_amount, commission, method, p_promo_code)
+        return await give_privilege_callback('Enot.io', p_uid, p_steam_link, new_amount, commission, method, p_promo_code)
     log.debug('request is wrong')
     return JSONResponse(content = {'error': 'Request is wrong'}, status_code = status.HTTP_403_FORBIDDEN)
+
+@router.post('/anypay', summary = 'Callback for anypay payments')
+async def index(
+    merchant_id: int = Form(), transaction_id: int = Form(default=0), pay_id: int = Form(), amount: int|float = Form(), currency: str = Form(),
+    profit: int|float = Form(default=None), email: str = Form(), method: str = Form(default="test"), pay_status: str = Form(alias='status'), test: int = Form(),
+    creation_date: str = Form(default='test'), completion_date: str = Form(default='test'),
+    sign: str = Form(), field1: str = Form()):
+    """Callback for anypay payments
+
+    Args:
+        merchant_id (int): ID вашего магазина  
+        transaction_id (int): ID платежа
+        pay_id (int): ID платежа в системе продавц
+        amount (int|float): Сумма заказа
+        currency (str): Валюта платежа
+        profit (int|float): Сумма зачисленная вам на баланс (В рублях)
+        email (str): Email плательщика
+        method (str): Способ оплаты
+        pay_status (str): Статус платежа в системе AnyPay: paid — оплачен
+        test (int): Тестовый платеж или нет (1 — тестовый, 0 — нет)
+        creation_date (str): Дата и время создания счета в формате DD.MM.YYYY hh:mm:ss
+        completion_date (str): Дата и время обработки счета в формате DD.MM.YYYY hh:mm:ss
+        sign (str): Ключ, который вы генерировали до оплаты заказа
+        field1 (str): Строка, которую передавали в параметр "custom_field"
+    """
+    if str(merchant_id) == os.environ.get('ANYPAY_SHOPID'):
+        if pay_status != 'paid':
+            log.debug('privilege not yes been paid')
+            return JSONResponse(content = {'error': 'Privilege has not yet been paid'}, status_code = status.HTTP_402_PAYMENT_REQUIRED)
+        server_sign = sign_md5(merchant_id, amount, pay_id, os.environ.get('ANYPAY_SECRET'))
+        if server_sign != sign:
+            log.debug('wrong sign')
+            return JSONResponse(content = {'error': 'Wrong SIGN'}, status_code = status.HTTP_403_FORBIDDEN)
+        if profit is not None:
+            commission = float(amount) - float(profit)
+        else:
+            commission = 0.0
+        p_uid = field1.split(',')[0]
+        p_amount = str(field1.split(',')[1])
+        if '.' in str(amount):
+            amount = str(amount).split('.')[0]
+        if str(amount) != str(p_amount):
+            log.debug('wrong amount')
+            return JSONResponse(content = {'error': 'Incorrect payment amount'}, status_code = status.HTTP_403_FORBIDDEN)
+        p_steam_link = field1.split(',')[2]
+        p_promo_code = field1.split(',')[3]
+        
+        return await give_privilege_callback('AnyPay', p_uid, p_steam_link, amount, commission, method, p_promo_code)
+    log.debug('request is wrong')
+    return JSONResponse(content = {'error': 'Request is wrong'}, status_code = status.HTTP_403_FORBIDDEN)
+
+@router.post('/lava', summary = 'Callback for lava payments')
+async def index(data: LavaRequest, signature = Header(default="", alias="Authorization")):
+    """Callback for lava payments"""
+    log.debug(locals())
+    if signature and data:
+        if data.status != 'success':
+            log.debug('privilege not yes been paid')
+            return JSONResponse(content = {'error': 'Privilege has not yet been paid'}, status_code = status.HTTP_402_PAYMENT_REQUIRED)
+        
+        secret2 = os.environ.get('LAVA_SECRET2')
+        server_sign = sign_hmac_sha256(sort_dict(data), secret2)
+
+        if server_sign != signature:
+            log.debug('wrong sign')
+            return JSONResponse(content = {'error': 'Wrong sign'}, status_code = status.HTTP_403_FORBIDDEN)
+
+        p_uid = data.custom_fields.split(',')[0]
+        p_amount = data.custom_fields.split(',')[1]
+        amount = data.amount
+        if '.' in str(amount):
+            amount = str(amount).split('.')[0]
+        if str(amount) != str(p_amount):
+            log.debug('wrong amount')
+            return JSONResponse(content = {'error': 'Incorrect payment amount'}, status_code = status.HTTP_403_FORBIDDEN)
+
+        if data.credited != data.amount:
+            commission = float(amount) - float(data.credited)
+        else:
+            commission = 0.0
+        p_steam_link = data.custom_fields.split(',')[2]
+        p_promo_code = data.custom_fields.split(',')[3]
+        return await give_privilege_callback('Lava', p_uid, p_steam_link, data.amount, commission, data.pay_service, p_promo_code)
+    return JSONResponse(content = {'error': 'Request is wrong'}, status_code = status.HTTP_403_FORBIDDEN)
+
+@router.post('/paypalych', summary = 'Callback for paypalych payments')
+async def index(
+    InvId: str = Form(), OutSum: float|int = Form(), Commission: float|int = Form(), TrsId: str = Form(), Status: str = Form(),
+    CurrencyIn: str = Form(), custom: str = Form(), SignatureValue: str = Form()):
+    """Callback for anypay payments
+
+    Args:
+        InvId (str): Уникальный идентификатор заказа, переданный при формировании счета
+        OutSum (float|int): Сумма платежа
+        Commission (float|int): Комиссия с платежа
+        TrsId (str): Уникальный идентификатор платежа 
+        Status (str): Статус платежа
+        CurrencyIn (str): Валюта, в которой оплачивался счет 
+        custom (str): Произвольное поле, переданное при формировании счета
+        SignatureValue (str): Подпись
+    """
+    if Status != 'SUCCESS':
+        log.debug('privilege not yes been paid')
+        return JSONResponse(content = {'error': 'Privilege has not yet been paid'}, status_code = status.HTTP_402_PAYMENT_REQUIRED)
+    server_sign = sign_md5_upper(OutSum, InvId, os.environ.get('PAYPALYCH_SECRET'))
+    if server_sign != SignatureValue:
+        log.debug('wrong sign')
+        return JSONResponse(content = {'error': 'Wrong SIGN'}, status_code = status.HTTP_403_FORBIDDEN)
+    p_uid = custom.split(',')[0]
+    p_amount = str(custom.split(',')[1])
+    if '.' in str(OutSum):
+        OutSum = str(OutSum).split('.')[0]
+    if str(OutSum) != str(p_amount):
+        log.debug('wrong amount')
+        return JSONResponse(content = {'error': 'Incorrect payment amount'}, status_code = status.HTTP_403_FORBIDDEN)
+    p_steam_link = custom.split(',')[2]
+    p_promo_code = custom.split(',')[3]
+    
+    return await give_privilege_callback('Paypalych', p_uid, p_steam_link, OutSum, Commission, 'Paypalych', p_promo_code)
