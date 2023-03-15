2e5f9d11a9f8bc1a41d0d120addb6e9dfb91f2ef
Toil <toil.official@yandex.com>
2023-03-16 01:01:27 +0300

    renamed privillege -> privilege, added payment systems

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from routes.endpoints import api_privilleges, api_promocodes, api_methods, api_callbacks
+from routes.endpoints import api_privileges, api_promocodes, api_methods, api_callbacks, api_payment_systems
 
 api_router = APIRouter(prefix = "/api")
-api_router.include_router(api_privilleges.router, tags = ['Privilleges'])
+api_router.include_router(api_privileges.router, tags = ['Privileges'])
 api_router.include_router(api_promocodes.router, tags = ['Promocodes'])
 api_router.include_router(api_methods.router, tags = ['Methods'])
 api_router.include_router(api_callbacks.router, tags = ['Callbacks'], prefix = "/callback")
+api_router.include_router(api_payment_systems.router, tags = ['Payment Systems'])
 
