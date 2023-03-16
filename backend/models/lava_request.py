d5206ddf0ad86f19977b7a988de4172b8b654b94
Toil <toil.official@yandex.com>
2023-03-17 01:48:29 +0300

    types fixes, added 'type' value

--- backend/models/lava_request.py
+++ backend/models/lava_request.py
@@ -3,11 +3,12 @@ from pydantic import BaseModel
 
 class LavaRequest(BaseModel):
     invoice_id: str
-    order_id: str
     status: str
     pay_time: str
-    amount: int|float
-    custom_fields: str
-    credited: str
+    amount: str
+    order_id: str
     pay_service: str
     payer_details: str
+    custom_fields: str
+    type: int
+    credited: str
