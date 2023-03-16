836788a476022e616dffa314d4a9bfcba339a777
Toil <toil.official@yandex.com>
2023-03-17 01:52:48 +0300

    fix for lava webhook validate

--- backend/utils/sign.py
+++ backend/utils/sign.py
@@ -12,6 +12,9 @@ def sign_md5_upper(amount: int|float, order_id: str, secret: str):
 def sign_sha1(pay_id: str, secret2: str):
     return sha1(f'{pay_id}:{secret2}'.encode('utf-8')).hexdigest()
 
-def sign_hmac_sha256(data: dict, secret: str):
-    json_str = json.dumps(data).encode()
+def sign_hmac_sha256(data: dict, secret: str, use_separators = False):
+    if use_separators:
+        json_str = json.dumps(data, separators=(',', ':')).encode()
+    else:
+        json_str = json.dumps(data).encode()
     return hmac.new(bytes(secret, 'UTF-8'), json_str, sha256).hexdigest()
