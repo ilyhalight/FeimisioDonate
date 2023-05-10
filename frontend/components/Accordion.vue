648d42feb05994b2d12032b17cc506588f51634a
unknown <toil.official@yandex.com>
2023-05-11 01:53:13 +0300

    added accordion component

---
+++ frontend/components/Accordion.vue
@@ -0,0 +1,30 @@
+<script setup>
+    const props = defineProps({
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    }
+  });
+</script>
+
+
+<template>
+    <details>
+      <summary>
+        <span class="summary-title">{{ title }}</span>
+        <div class="summary-icon-hidden">
+          <Icon name="fluent:chevron-down-24-filled"/>
+        </div>
+      </summary>
+      <div class="summary-content">
+        <slot name="content"></slot>
+      </div>
+      <div class="summary-icon-opened">
+        <Icon name="fluent:chevron-up-24-filled"/>
+      </div>
+  </details>
+</template>
