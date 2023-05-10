d56739ff777b6ff5d9cbbc9eb684899b3cd811ca
unknown <toil.official@yandex.com>
2023-05-11 01:50:54 +0300

    added debug logs

--- backend/utils/token.py
+++ backend/utils/token.py
@@ -1,6 +1,9 @@
 import os
 import time
+import logging
 from hashlib import sha256
+
+logger = logging.getLogger('server')
 
 
 async def generate_token(string: str, timestamp: int):
@@ -9,9 +12,11 @@ async def generate_token(string: str, timestamp: int):
     return token
 
 async def validate_token(token: str, string: str, timestamp: int):
-    if int(time.time()) > timestamp + 12:
+    if int(time.time()) > timestamp + 30:
+        logger.debug(f'Timestamp is older than current timestamp (30+ seconds ago | api: {int(time.time())}, request: {timestamp + 30} )')
         return False
     token_generated = await generate_token(string, timestamp)
     if token == token_generated:
+        logger.debug('The token matches')
         return True
     return False
